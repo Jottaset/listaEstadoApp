5885f25f174bd2b4f4121b986fc2cedf21614f1b
JAMES FREITAS <j7@Mac-mini-de-JAMES.local>
2019-03-26 16:54:52 -0400

    ínicio

---
+++ listaEstadoApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace listaEstadoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+
+        }
+
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
